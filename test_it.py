fa96bf20d1d92253b5358c081be64ecba03ccc42
Tomas Trnka <tomas@trnkatomas.eu>
2022-11-11 23:38:43 +0100

    test endpoint and enabled cors from the local server

--- test_it.py
+++ test_it.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import random
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+origins = [    
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/test_endpoint")
+def js_endpoint(input: str):
+    print(input)
+    return {'hello': random.randint(0,100)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
